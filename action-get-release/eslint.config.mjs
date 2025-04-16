93ba9370cf50164721835479c96d913f26922f5e
romnn <roman.dahm@gmx.de>
2025-04-17 00:16:44 +0200

    fix linting

--- action-get-release/eslint.config.mjs
+++ action-get-release/eslint.config.mjs
@@ -4,7 +4,12 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    ignores: ["dist/**/*"],
+  },
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts}"],
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
