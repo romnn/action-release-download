3710c2973b3e13b000bcf0eba211ace79a9f0403
romnn <roman.dahm@gmx.de>
2026-07-24 20:28:24 +0200

    test(github): parse owner/repo from version-tagged refs

--- action-get-release/tests/github.spec.ts
+++ action-get-release/tests/github.spec.ts
@@ -24,4 +24,22 @@ describe("github", () => {
     });
     expect(() => parseGithubRepo("some-repo")).toThrow(Error);
   });
+
+  it("extracts owner/repo from refs carrying a version tag", () => {
+    const expected = { repo: "some-repo", owner: "some-user" };
+    expect(parseGithubRepo("some-user/some-repo/releases/tag/v1.2.3")).toEqual(
+      expected,
+    );
+    expect(
+      parseGithubRepo(
+        "https://github.com/some-user/some-repo/releases/tag/v0.0.39",
+      ),
+    ).toEqual(expected);
+    expect(
+      parseGithubRepo("https://github.com/some-user/some-repo/tree/v2.0.0"),
+    ).toEqual(expected);
+    expect(
+      parseGithubRepo("some-user/some-repo/releases/download/v1.0.0/asset.zip"),
+    ).toEqual(expected);
+  });
 });
